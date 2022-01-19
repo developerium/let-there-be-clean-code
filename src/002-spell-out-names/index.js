4f5cade346a8ae3f81366d9370da7ebd02b23959
developerium <kheradmand.v1@gmail.com>
2022-01-19 20:48:04 +0100

    code 002 spell out names

---
+++ src/002-spell-out-names/index.js
@@ -0,0 +1,24 @@
+/**
+ * You are reading a function and you encounter
+ * acronyms that are not standard...
+ *
+ * It's impossible to understand these variables fully
+ * without reading the whole block of code
+ */
+
+let ndx;
+
+let lstAcsDte;
+
+let remAcCrd;
+
+/**
+ * Spell them out, and let the reader understand
+ * way more quickly
+ */
+
+let index;
+
+let lastAccessDate;
+
+let remainingAccountCredit;
