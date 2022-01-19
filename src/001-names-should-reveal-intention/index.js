18ccfca567521e0edeaa425b25488b93f2e4f01f
developerium <kheradmand.v1@gmail.com>
2022-01-19 20:47:51 +0100

    code 001 names should reveal intention

---
+++ src/001-names-should-reveal-intention/index.js
@@ -0,0 +1,47 @@
+/**
+ * Can you tell what does this variable actually hold?
+ * What is it's purpose?
+ */
+
+let m;
+
+/**
+ * How about now?
+ * It's better than before, but still quite vague...
+ */
+
+let max;
+
+/**
+ * What if we want this variable to hold
+ * "Maximum available credit"?
+ * Wouldn't it be better if we reflected the intent in the variable name?
+ */
+
+let maximumAvailableCredit;
+
+/**
+ * More examples
+ * Please note that name of these variables are self explainatory
+ * and doesn't need additional comments
+ */
+
+let daysSinceModification;
+
+let lastAccessDate;
+
+let textParserOptions;
+
+let decodedToken;
+
+let maxAgeInSeconds;
+
+/**
+ * Function names must follow this rule as well
+ */
+
+function calculateRemainingCredit() {}
+
+function getLastModifer() {}
+
+function processNextUser() {}
