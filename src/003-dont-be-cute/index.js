af0ebb1e4f9ca1bab4268ef75255240ed109990e
developerium <kheradmand.v1@gmail.com>
2022-01-24 17:25:27 +0100

    code 003 don't be cute

---
+++ src/003-dont-be-cute/index.js
@@ -0,0 +1,39 @@
+// define to make linter happy :)
+const releaseTheKraken = () => {};
+const deleteAllRecords = () => {};
+const mikesLunch = () => {};
+const whack = () => {};
+const letThereBeCarnage = () => {};
+
+/**
+ * Reading the code below what do you understand?
+ */
+
+releaseTheKraken();
+
+/**
+ * With a little refactor, What about now?
+ *
+ * It now clearly says what it's doing
+ */
+
+deleteAllRecords();
+
+/**
+ * Code should not rely on internal jokes
+ */
+
+mikesLunch();
+
+/**
+ * Don't use slangs
+ */
+
+whack();
+
+/**
+ * Don't use things that are not related to
+ * the problem domain
+ */
+
+letThereBeCarnage();
